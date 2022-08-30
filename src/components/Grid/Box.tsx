615c640cc9ce19a380e73048aeb50f62f747ee90
Adam Andersson <adander@kth.se>
2022-08-31 00:04:21 +0200

    Fix highlighting of absent characters

--- src/components/Grid/Box.tsx
+++ src/components/Grid/Box.tsx
@@ -19,13 +19,17 @@ const Box: FC<{
 				marginRight: '0.15rem',
 				marginLeft: '0.15rem',
 				display: 'flex',
+				flexDirection: 'column',
 				alignItems: 'center',
 				justifyContent: 'center',
 				borderRadius: '0.5rem',
-				fontSize: '25px'
+				fontSize: '25px',
+				position: 'relative'
 			}}
 		>
 			{character?.toUpperCase()}
+			<span style={{position: 'absolute', bottom: '8px'}}>{isSelectedBox && '_'}</span>
+			
 		</div>
 	)
 }
--- src/helpers/get-box-color.ts
+++ src/helpers/get-box-color.ts
@@ -2,8 +2,9 @@ import { CharStatus } from '../lib/word-logic'
 
 export const getBoxColor = (status?: CharStatus, isSelectedBox?: boolean, isActiveGuessInvalid?: boolean) => {
 	if (isActiveGuessInvalid) return '#fb6a75'
-	if (isSelectedBox) return 'rgb(32,32,32)'
+	if (isSelectedBox) return 'rgb(48,48,48)'
 	if (status === 'correct') return '#00c7b7'
 	if (status === 'present') return '#f8bc00'
+	if (status === 'absent') return 'rgb(32,32,32)'
 	return 'rgb(64,64,64)'
 }
